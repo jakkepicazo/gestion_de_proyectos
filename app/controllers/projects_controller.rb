2a704fd0258eeb281bb937d6a370b28e96251fa9
Jakke Picado <jakkepicazo@gmail.com>
2021-03-04 16:59:38 -0600

    finish

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,34 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+    if params[:status] and params[:status] != 'all'
+      @projects = @projects.where(status: params[:status])
+    end
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to dashboard_path
+    else
+      render 'new'
+    end
+  end
+  
+  def destroy
+    @project = Project.find(params[:id])
+    if @project.destroy
+      redirect_to dashboard_path
+    end
+  end
+
+  private
+  # Strong params
+  def project_params
+    params.require(:project).permit(:name, :description, :started_at, :ended_at, :status)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'dashboard' => 'projects#index'
+  get 'projects/new' => 'projects#new'
+  post 'projects' => 'projects#create'
+  delete 'projects/:id' => 'projects#destroy', as: 'destroy_project'
+  root 'projects#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
